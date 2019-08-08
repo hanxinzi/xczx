912c8a47784d9b47ffdc8832f553d38aace60bab
韩歆子 <237891672@qq.com>
2019-08-08 14:29:05 +0800

    关于转json的描述

---
+++ xc-framework-common/src/main/java/com/xuecheng/framework/exception/B.java
@@ -0,0 +1,24 @@
+package com.xuecheng.framework.exception;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Hanxinzi
+ * @date 2019/8/8
+ */
+public class B {
+    public static void main(String[] args) {
+        List<String> jsonList = new ArrayList<>();
+        jsonList.add("aaa");
+        jsonList.add("bbb");
+        jsonList.add("ccc");
+
+    }
+   /* [{"aaa"},{"bbb"},{"ccc"}]
+    {{'id':"123"},{'name':"s"}}*/
+
+}
